c3ea9bd19609e48d27072c70bdce7eca19a3915e
mikibak <mikibak123@gmail.com>
2023-05-28 16:18:46 +0200

    Change C based on experimental results

--- svm.py
+++ svm.py
@@ -5,7 +5,7 @@ from sklearn.svm import SVC
 class Svm:
 
     def __init__(self):
-        self.svm = SVC(kernel='linear', random_state=1, C=0.1)
+        self.svm = SVC(kernel='linear', random_state=1, C=0.05)
     def train(self, X, y):
         # Training a SVM classifier using SVC class
         self.svm.fit(X, y)
@@ -13,3 +13,4 @@ class Svm:
     def evaluate(self, X, y):
         y_pred = self.svm.predict(X)
         print('Accuracy: %.3f' % accuracy_score(y, y_pred))
+        return accuracy_score(y, y_pred)
