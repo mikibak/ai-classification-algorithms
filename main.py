d775ca0bbfa2f97e8a2a620071fc928e78432ce0
mikibak <mikibak123@gmail.com>
2023-05-29 14:48:15 +0200

    Add time plot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import svm
+import time
 import NeuralNetwork
 from Confusion_matrix import ConfusionMatrix
 from decision_tree import DecisionTree
@@ -10,21 +11,25 @@ from load_data import generate_data, load_titanic, load_reviews
 
 def main():
 
-
     confMatrix = ConfusionMatrix()
     np.random.seed(123)
-    data_sizes = [50, 100, 200, 500, 1000]
+    data_sizes = [50, 100, 200, 500, 1000, 2000, 5000]
     #data_sizes = [10000]
     train_accuracy_svm = []
     test_accuracy_svm = []
     test_accuracy_NN = []
     train_accuracy_NN = []
+    time_SVM = []
+    time_NN = []
 
     for data_size in data_sizes:
         # train_data, test_data = load_titanic()
         train_data, test_data = load_reviews(data_size)
+        start = time.time()
         NN = NeuralNetwork.NeuraltNetwork()
         NN.train(train_data, test_data)
+        end = time.time()
+        time_NN.append(end - start)
         test_accuracy_NN.append(NN.evaluate(*test_data))
         train_accuracy_NN.append(NN.evaluate(*train_data))
         confMatrix.addNN(NN.getPredictions(test_data[0]), test_data[1])
@@ -33,8 +38,11 @@ def main():
         # dt.evaluate(*train_data)
         # dt.evaluate(*test_data)
 
+        start = time.time()
         SVM = svm.Svm()
         SVM.train(*train_data)
+        end = time.time()
+        time_SVM.append(end - start)
         confMatrix.addSvn(SVM.getPredictions(test_data[0]),test_data[1])
         train_accuracy_svm.append(SVM.evaluate(*train_data))
         test_accuracy_svm.append(SVM.evaluate(*test_data))
@@ -47,7 +55,7 @@ def main():
     print(train_accuracy_NN)
     print(test_accuracy_NN)
 
-    plt.title("Train and test accuracy for SVM")
+    plt.title("Train and test accuracy for SVM (binary)")
     plt.xlabel('Data size (number of opinions)')
     plt.ylabel('Accuracy')
     plt.subplots_adjust(bottom=0.25)
@@ -56,7 +64,7 @@ def main():
     plt.legend(loc='best')
     plt.show()
 
-    plt.title("Train and test accuracy for NN")
+    plt.title("Train and test accuracy for NN (binary)")
     plt.xlabel('Data size (number of opinions)')
     plt.ylabel('Accuracy')
     plt.subplots_adjust(bottom=0.25)
@@ -65,7 +73,7 @@ def main():
     plt.legend(loc='best')
     plt.show()
 
-    plt.title("Test accuracy for SVM and NN")
+    plt.title("Test accuracy for SVM and NN (binary)")
     plt.xlabel('Data size (number of opinions)')
     plt.ylabel('Accuracy')
     plt.subplots_adjust(bottom=0.25)
@@ -74,12 +82,21 @@ def main():
     plt.legend(loc='best')
     plt.show()
 
-    plt.title("Train accuracy for SVM and NN")
+    plt.title("Train accuracy for SVM and NN (binary)")
     plt.xlabel('Data size (number of opinions)')
     plt.ylabel('Accuracy')
     plt.subplots_adjust(bottom=0.25)
     plt.plot(data_sizes, train_accuracy_svm, label="Train accuracy SVM")
     plt.plot(data_sizes, train_accuracy_NN, label="Train accuracy NN")
+    plt.legend(loc='best')
+    plt.show()
+
+    plt.title("Time of execution for SVM and NN (binary)")
+    plt.xlabel('Data size (number of opinions)')
+    plt.ylabel('Time [s]')
+    plt.subplots_adjust(bottom=0.25)
+    plt.plot(data_sizes, time_SVM, label="Time of SVM")
+    plt.plot(data_sizes, time_NN, label="Time of NN")
     plt.legend(loc='best')
     plt.show()
 
